911b73faee0e144d49cdf34667b9c31ff4ad2a65
tommysft <thgaertner.sft@t-online.de>
2021-05-07 13:04:46 +0200

    Update Method g estimation

--- R/g_estimation.R
+++ R/g_estimation.R
@@ -12,10 +12,11 @@
 #' @param exposure string, defines the exposure variable
 #' @param confounder string vector, defines the confounder variables
 #' @param id string, identify the unique identifier
-calc_beta <- function(data, psi, outcome, exposure, confounder, id){
+#' @param corstr a character string specifying the correlation structure. The following are permitted: '"independence"', '"exchangeable"', '"ar1"', '"unstructured"' and '"userdefined"'
+calc_beta <- function(data, psi, outcome, exposure, confounder, id, corstr = "independence"){
   data$Hpsi <- as.numeric(data[,outcome])-psi*as.numeric(data[,exposure])
   str_formula <- sprintf("%s ~ %s", exposure, paste(confounder, "Hpsi", sep=" + ", collapse = " + "))
-  g.est <- geepack::geeglm(as.formula(str_formula), family=gaussian, data=data, id = data[[id]], corstr="independence")
+  g.est <- geepack::geeglm(as.formula(str_formula), family=gaussian, data=data, id = data[[id]], corstr=corstr)
   beta <- summary(g.est)$coefficients["Hpsi","Estimate"]
   se <- summary(g.est)$coefficients["Hpsi","Std.err"]
   return(list(beta = beta, se = se))
@@ -29,7 +30,10 @@ calc_beta <- function(data, psi, outcome, exposure, confounder, id){
 #' @param exposure exposure variable name
 #' @param confounder list of confounders
 #' @param id patient id
-#' @param max_number_it max umber of iterations
+#' @param max_number_it max number of iterations
+#' @param steps number of steps if iteration
+#' @param corstr a character string specifying the correlation structure. The following are permitted: '"independence"', '"exchangeable"', '"ar1"', '"unstructured"' and '"userdefined"'
+#' @param verbose Boolean, if prelimnary results should be printed
 #' @examples
 #' load(simpatdat)
 #' nofgest()
@@ -45,6 +49,7 @@ nofgest <- function(
   lower_bound_psi=-2,
   max_number_it=NULL,
   steps=10,
+  corstr = 'independence',
   verbose=TRUE){
 
   # handle missing values
@@ -69,7 +74,8 @@ nofgest <- function(
                                 id,
                                 upper_bound_psi,
                                 lower_bound_psi,
-                                steps))
+                                steps,
+                                corstr = corstr))
   }else{
     res <- calc_beta(data = data, psi = upper_bound_psi, outcome, exposure, confounder, id=id)
     upper_bound_beta <- res$beta
@@ -95,7 +101,8 @@ nofgest <- function(
                      max_number_it,
                      epsilon,
                      method=method,
-                     verbose=verbose)
+                     verbose=verbose,
+                     corstr = corstr)
 
     df[nrow(df) + 1,] <- c(upper_bound_psi, upper_bound_beta, upper_se, lower_bound_psi, lower_bound_beta, lower_se, number_it)
     return(df)
@@ -106,15 +113,18 @@ nofgest <- function(
 
 #' Find Optimum Recursively
 #' @description This function finds the best psi value recursively
-#' @param data
-#' @param outcome
-#' @param exposure
-#' @param confounder
-#' @param id
-#' @param number_it
-#' @param upper_bound_psi
-#' @param lower_bound_psi
-#' @param max_number_it
+#' @param data data frame with patients
+#' @param outcome outcome string
+#' @param exposure exposure string
+#' @param confounder list of confounders
+#' @param id id variable
+#' @param number_it number of iteration (current)
+#' @param upper_bound_psi double, upper bound psi
+#' @param lower_bound_psi double, lower bound psi
+#' @param max_number_it number max iterations
+#' @param epsilon target epsilon
+#' @param corstr a character string specifying the correlation structure. The following are permitted: '"independence"', '"exchangeable"', '"ar1"', '"unstructured"' and '"userdefined"'
+#' @param verbose boolean for printing
 find_optimum_rec <- function(
   data,
   outcome,
@@ -131,6 +141,7 @@ find_optimum_rec <- function(
   max_number_it,
   epsilon,
   method,
+  corstr = '"independence"',
   verbose=TRUE){
 
 
@@ -184,7 +195,13 @@ find_optimum_rec <- function(
     new_psi <- -a/m
   }
 
-  res <- calc_beta(data = data, psi = new_psi, outcome, exposure, confounder, id)
+  res <- calc_beta(data = data,
+                   psi = new_psi,
+                   outcome = outcome,
+                   exposure = exposure,
+                   confounder = confounder,
+                   id = id,
+                   corstr = corstr)
 
   new_beta <- res$beta
   new_se <- res$se
@@ -217,6 +234,7 @@ find_optimum_rec <- function(
                          max_number_it = max_number_it,
                          epsilon = epsilon,
                          method=method,
+                         corstr = corstr,
                          verbose = verbose)
   df[nrow(df) + 1,] <-  c(upper_bound_psi, upper_bound_beta, upper_se, lower_bound_psi, lower_bound_beta, lower_se, number_it)
   return(df)
@@ -227,15 +245,16 @@ find_optimum_rec <- function(
 
 #' Find Optimum Brute Force
 #' @description This function finds the best psi value brute forced
-#' @param data
-#' @param outcome
-#' @param exposure
-#' @param confounder
-#' @param id
-#' @param number_it
-#' @param upper_bound_psi
-#' @param lower_bound_psi
-#' @param max_number_it
+#' @param data data frame with patient data
+#' @param outcome  outcome string
+#' @param exposure exposure string
+#' @param confounder list of confounders
+#' @param id id variable
+#' @param number_it current iteration
+#' @param upper_bound_psi upper psi
+#' @param lower_bound_psi lower psi
+#' @param steps steps between lower and upper psi
+#' @param corstr a character string specifying the correlation structure. The following are permitted: '"independence"', '"exchangeable"', '"ar1"', '"unstructured"' and '"userdefined"'
 find_optimum_iterate <- function(data,
                                  outcome,
                                  exposure,
@@ -243,7 +262,8 @@ find_optimum_iterate <- function(data,
                                  id,
                                  upper_bound_psi,
                                  lower_bound_psi,
-                                 steps){
+                                 steps,
+                                 corstr = "independence"){
 
   psis <- seq(upper_bound_psi, lower_bound_psi, length = steps)
   betas <- c()
@@ -257,7 +277,7 @@ find_optimum_iterate <- function(data,
 
   df <- foreach::foreach(i = c(1:length(psis)), .combine ="rbind") %dopar% {
     psi <- psis[[i]]
-    res <- calc_beta(data = data, psi = psi, outcome, exposure, confounder, id)
+    res <- calc_beta(data = data, psi = psi, outcome, exposure, confounder, id, corstr = corstr)
     beta <- res$beta
     se <- res$se
     c(psi, beta, se)
